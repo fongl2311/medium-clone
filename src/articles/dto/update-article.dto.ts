0648733ac43ec31ff85a0bab059fcd7f32648eb5
phongn <mycloud1235@gmail.com>
2025-07-24 10:08:09 +0700

    create crud-articles feature

---
+++ src/articles/dto/update-article.dto.ts
@@ -0,0 +1,8 @@
+import { IsArray, IsOptional, IsString } from 'class-validator';
+
+export class UpdateArticleDto {
+  @IsOptional() @IsString() title?: string;
+  @IsOptional() @IsString() description?: string;
+  @IsOptional() @IsString() body?: string;
+  @IsOptional() @IsArray() @IsString({ each: true }) tagList?: string[];
+}
